76f055cd20737be6f6459ed47b33551e5da64d08
hoangduy <duyunh@mapmaker.asia>
2020-08-31 16:36:50 +0700

    first commit

---
+++ WebApplication1/Data/MvcStudentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class MvcStudentContext : DbContext
+    {
+        public MvcStudentContext(DbContextOptions<MvcStudentContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public char Name { get; set; }
+
+        public char Type { get; set; }
+    }
+}
